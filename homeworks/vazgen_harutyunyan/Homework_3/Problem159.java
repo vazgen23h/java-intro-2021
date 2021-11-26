57a542a115ef9c237788d82228a0a8bed4767c52
vazgen23h <94632145+vazgen23h@users.noreply.github.com>
2021-11-26 23:55:45 +0400

    Update Problem159.java

--- homeworks/vazgen_harutyunyan/Homework_3/Problem159.java
+++ homeworks/vazgen_harutyunyan/Homework_3/Problem159.java
@@ -1,21 +1,12 @@
 public class Problem159 {
     public static void main(String[] args) {
-        long product = 1L; //
-        int firstNumber = 100;
-        int arrayLength = 900;
-        int[] array = new int[arrayLength];
-        for(int i = 0; i < 900; i++) {
-            array[i] = firstNumber;
-            firstNumber++;
+        long product = 1L;
+        for(int i=100; i<1000; i++){
+            if (i%3==1 && i%4==2){
+                product=product*i
         }
-
-        for(int i = 0; i < 900; i++) {
-            if(array[i] % 3 == 1 || array[i]%4==2 ) {
-                product = product * array[i];
-            }  
-        }
+}
         System.out.println(product);
-        //Product out of range Long variable type
     }
     
 }
