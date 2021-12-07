11da6cac699e3b3711f976493c19b2cecb9146b4
vazgen23h <94632145+vazgen23h@users.noreply.github.com>
2021-12-07 13:57:37 +0400

    Update Power.java

--- homeworks/vazgen_harutyunyan/Homework_7/Power.java
+++ homeworks/vazgen_harutyunyan/Homework_7/Power.java
@@ -1,11 +1,8 @@
 public class Power{
 	public static void main(String[] args){
-
  System.out.println(power(2,3));
-
 	}
-
-
+	
 	static int power(int number, int power){
 		int p=1;
 		while(power>0){
