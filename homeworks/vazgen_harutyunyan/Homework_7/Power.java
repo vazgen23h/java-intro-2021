f575d58656dc1b517acf3d998e05802005047bb3
vazgen23h <94632145+vazgen23h@users.noreply.github.com>
2021-12-07 14:56:12 +0400

    Update Power.java

--- homeworks/vazgen_harutyunyan/Homework_7/Power.java
+++ homeworks/vazgen_harutyunyan/Homework_7/Power.java
@@ -1,14 +1,13 @@
-public class Power{
-	public static void main(String[] args){
- System.out.println(power(2,3));
-	}
-	
-	static int power(int number, int power){
-		int p=1;
-		while(power>0){
-			p=p*number;
-			power--;
-		}
-		return p;
-	}
+public class Power {
+    public static void main(String[] args) {
+        System.out.println(power(2, 3));
+    }
+
+    static int power(int number, int power) {
+        if(power != 0) {
+            return number * power(number, power - 1);
+        } else {
+            return 1;
+        }
+    }
 }
