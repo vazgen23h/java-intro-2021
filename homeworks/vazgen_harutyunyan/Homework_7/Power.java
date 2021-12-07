6e3b7dbabea8adf4d4dc870e6035c77de5b0c489
Vazgen Harutyunyan <v.haroutyunyan23@gmail.com>
2021-12-07 13:21:40 +0400

    Homework 7 without DigitToWork

---
+++ homeworks/vazgen_harutyunyan/Homework_7/Power.java
@@ -0,0 +1,17 @@
+public class Power{
+	public static void main(String[] args){
+
+ System.out.println(power(2,3));
+
+	}
+
+
+	static int power(int number, int power){
+		int p=1;
+		while(power>0){
+			p=p*number;
+			power--;
+		}
+		return p;
+	}
+}
