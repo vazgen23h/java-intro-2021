291ff2431bf35f841bc76fb1e4ef7886f11de79c
Vazgen Harutyunyan <v.haroutyunyan23@gmail.com>
2021-12-09 14:09:59 +0400

    Homework_8 without Problem5

---
+++ homeworks/vazgen_harutyunyan/Homework_8/Problem4.java
@@ -0,0 +1,8 @@
+public class Problem4 {
+    public static void main(String[] args) {
+        String str1 = "Hello World";
+        String str2 = "Hello";
+        boolean a = str1.startsWith(str2);
+        System.out.println(a);
+    }
+}
